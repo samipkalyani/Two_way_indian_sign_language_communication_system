a63982fc53c0234069895d7c740e51689d18ac20
samip kalyani <samipkalyani@gmail.com>
2021-03-05 10:08:42 +0530

    basic_boilerplate

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, abort
+import os
+from dotenv import load_dotenv
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VideoGrant, ChatGrant
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
+
+load_dotenv()
+twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
+twilio_api_key_secret = os.environ.get('TWILIO_API_KEY_SECRET')
+twilio_client = Client(twilio_api_key_sid, twilio_api_key_secret,twilio_account_sid)
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
